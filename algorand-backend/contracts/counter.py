b2805d908a88dfb89af3290c0bfe6c93ae8e6c2c
ameetadhikari <75064957+ameetadhikari@users.noreply.github.com>
2024-04-24 23:23:43 +0000

    Smart contracts

---
+++ algorand-backend/contracts/counter.py
@@ -0,0 +1,10 @@
+from pyteal import *
+import os
+"""Basic Counter Application"""
+def approval_program():
+program = Return(Int(1))
+return compileTeal(program, Mode.Application, version=5)
+def clear_state_program():
+program = Return(Int(1))
+return compileTeal(program, Mode.Application, version=5)
+print(approval_program());
